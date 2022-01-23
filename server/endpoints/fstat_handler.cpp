4ff6d72f6ac38d5232a8ee5ae65a6007eaef072b
mbrus <brus.michal@outlook.com>
2022-01-23 20:29:16 +0100

    get_stat: variable name changed

--- server/endpoints/fstat_handler.cpp
+++ server/endpoints/fstat_handler.cpp
@@ -6,7 +6,7 @@ FstatHandler::FstatHandler(FstatRequest &fstat_request) :
         path(fstat_request.get_path()) {}
 
 std::string FstatHandler::get_fstat() {
-    std::string stats;
+    std::string styled_fstat_info;
     try {
         AccessManager::get_instance().lock_file_mutex(path);
     } catch (std::runtime_error &error) {
@@ -18,14 +18,14 @@ std::string FstatHandler::get_fstat() {
         throw std::runtime_error("ERROR IN: file_to_fstat - file couldn't be opened");
     }
     try {
-        load_fstats(stats, file_to_fstat);
+        load_fstats(styled_fstat_info, file_to_fstat);
     }
     catch (std::runtime_error &error) {
         throw std::runtime_error("ERROR IN: load_fstats: " + std::string(error.what()));
     }
     fclose(file_to_fstat);
     AccessManager::get_instance().unlock_file_mutex(path);
-    return stats;
+    return styled_fstat_info;
 }
 
 std::string FstatHandler::load_fstats(std::string &stats, FILE *file_to_fstat) {
