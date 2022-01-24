c9b6fa030fb5f9195fc84a9b7de63b0ba4a27e21
mbrus <brus.michal@outlook.com>
2022-01-25 00:02:44 +0100

    error catching added, logger works now

--- server/logger.h
+++ server/logger.h
@@ -10,24 +10,26 @@
 
 class Logger {
 private:
-
-    Logger();
-
     std::string file_path;
     std::ofstream logs_file;
     std::mutex logs_mutex;
-//    std::vector<std::thread> thread_pool;
-//    std::shared_ptr<Logger> logger;
-//    int thread_counter;
-public:
-    static Logger &get_instance();
+
+    Logger();
+
     ~Logger();
 
-    Logger(Logger const &) = delete;
-    void operator=(Logger const &) = delete;
-    void create_new_log(const std::string& log_info, std::thread::id thread_id);
     void unlock_logs_mutex();
+
     void lock_logs_mutex();
+
+public:
+    static Logger &get_instance();
+
+    Logger(Logger const &) = delete;
+
+    void operator=(Logger const &) = delete;
+
+    void create_new_log(const std::string &log_info, std::thread::id thread_id);
 };
 
 
