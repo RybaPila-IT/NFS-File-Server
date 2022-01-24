cf3ad165bfd0e08578e12c4b62f7efc68d838204
mbrus <brus.michal@outlook.com>
2022-01-25 00:02:44 +0100

    error catching added, logger works now

--- server/logger.cpp
+++ server/logger.cpp
@@ -6,20 +6,25 @@ Logger &Logger::get_instance() {
     return logger;
 }
 
-Logger::Logger(): file_path("logs.txt"){
-    this->logs_file.open(this->file_path,std::fstream::out | std::fstream::trunc);
-
+Logger::Logger() : file_path("logs.txt") {
+    try {
+        this->logs_file.open(this->file_path, std::fstream::out | std::fstream::trunc);
+    } catch (std::runtime_error &error) {
+        throw std::runtime_error("ERROR IN: open file in logger constructor: " + std::string(error.what()));
+    }
 }
 
-void Logger::create_new_log(const std::string& log_info, std::thread::id thread_id) {
+void Logger::create_new_log(const std::string &log_info, std::thread::id thread_id) {
     lock_logs_mutex();
     try {
         std::stringstream id_stream;
         id_stream << thread_id;
         std::string id_string = id_stream.str();
-        this->logs_file << "Thread id: " + id_string + ". " + log_info;
+        this->logs_file << "Thread id: " + id_string + ". " + log_info << std::endl;
+        //std::this_thread::sleep_for(std::chrono::seconds(10));
     }
     catch (std::runtime_error &error) {
+        unlock_logs_mutex();
         throw std::runtime_error("ERROR IN: create_new_log: " + std::string(error.what()));
     }
     unlock_logs_mutex();
