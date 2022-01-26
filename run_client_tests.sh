c3546a3de57940b0837b33df5de434cd583b142e
IG-G <igolos@op.pl>
2022-01-26 22:13:29 +0100

    TMP directory for execution and comment update

--- run_client_tests.sh
+++ run_client_tests.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
-# Ten skrypt umożliwia uruchomienie programu testowego klienta z domyślnymi parametrami
+
+# Ten skrypt umożliwia uruchomienie testowego programu klienckiego
+# W przypadku braku argumentów lub tylko z jednym argumentem zastąpi on brakujące domyslnym wartościami:
+# IP = 127.0.0.1
+# PORT = 6941
+
 client_test_program/build/client_test_program $1 $2
--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
-# Ten skrypt umożliwia uruchomienie serwera z domyślnymi parametrami
+
+# Ten skrypt umożliwia uruchomienie serwera
+# W przypadku braku argumentów lub tylko z jednym argumentem zastąpi on brakujące domyslnym wartościami:
+# IP = 127.0.0.1
+# PORT = 6941
+
+if [ ! -d "build" ]; then
+    mkdir tmp
+fi
+cd tmp
 ls | grep .test_file | xargs rm -f
-server/build/server $1 $2
+../server/build/server $1 $2
 ls | grep .test_file | xargs rm -f
+cd ..
