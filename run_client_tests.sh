cd1428626854b0753a2cd86fd2e77a116295db6c
IG-G <igolos@op.pl>
2022-01-26 21:24:45 +0100

    Removing .test_file should be on server side

--- run_client_tests.sh
+++ run_client_tests.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 # Ten skrypt umożliwia uruchomienie programu testowego klienta z domyślnymi parametrami
-ls | grep .test_file | xargs rm -f
 client_test_program/build/client_test_program $1 $2
-ls | grep .test_file | xargs rm -f
--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 # Ten skrypt umożliwia uruchomienie serwera z domyślnymi parametrami
+ls | grep .test_file | xargs rm -f
 server/build/server $1 $2
+ls | grep .test_file | xargs rm -f
