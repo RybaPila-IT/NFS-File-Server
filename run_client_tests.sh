936a92c279d332d823ed71e1d65c78be1e99b746
IG-G <igolos@op.pl>
2022-01-26 20:19:07 +0100

    Added script args for ip and port number

--- run_client_tests.sh
+++ run_client_tests.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # Ten skrypt umożliwia uruchomienie programu testowego klienta z domyślnymi parametrami
 ls | grep .test_file | xargs rm -f
-client_test_program/build/client_test_program
+client_test_program/build/client_test_program $1 $2
 ls | grep .test_file | xargs rm -f
